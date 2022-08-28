9c1aaa49f93cb27d8a28a753f0fe8320dc088e8e
shahfahad19 <sfahad4280@gmail.com>
2022-08-28 17:34:13 +0500

    ok

--- index.js
+++ index.js
@@ -2,8 +2,18 @@ const express = require("express");
 const axios = require("axios");
 const app = express();
 
+app.use(express());
+
 app.get("/", (req, res) => {
-    res.send("HEllo");
+    const url = req.query.url | "";
+    axios
+        .get(url)
+        .then((response) => {
+            res.send(response.data);
+        })
+        .catch((error) => {
+            res.send(error.message);
+        });
 });
 
 app.listen(process.env.PORT || 5000, () => console.log("Server is running..."));
