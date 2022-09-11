38e14941d1d404e3d5ec5c638b58937bedc8289b
shahfahad19 <sfahad4280@gmail.com>
2022-09-11 11:47:24 +0500

    CHNAGE

--- index.js
+++ index.js
@@ -1,19 +1,43 @@
+// create an express app
 const express = require("express");
 const axios = require("axios");
 const app = express();
 
 app.use(express());
 
-app.get("/", (req, res) => {
-    const url = req.query.url | "";
+app.get("/c/:channel", function (req, res) {
+    let channel = req.params.channel || "ptv-sports";
     axios
-        .get(url)
-        .then((response) => {
-            res.send(response.data);
+        .get("https://streamia.xyz/embed/" + channel, {
+            headers: {
+                Referer: "https://sportshd.co"
+            }
         })
-        .catch((error) => {
-            res.send(error.message);
+        .then(function (response) {
+            let data = response.data;
+            data = data.replace(
+                `</style>`,
+                `
+		.container:after {
+            content: "Website made by Shah Fahad";
+            position: absolute;
+            bottom: 0;
+            width: 100vw;
+            font-size: 12px;
+            right: 0;
+            color: rgba(255, 255, 255, 0.5);
+            z-index: 99;
+            font-family: Arial, Helvetica, sans-serif;
+		}
+			</style>`
+            );
+            res.send(data);
+        })
+        .catch(function (err) {
+            res.send(err.message);
         });
 });
 
-app.listen(process.env.PORT || 5000, () => console.log("Server is running..."));
+app.listen(process.env.PORT || 5000, () =>
+    console.log("SERVER STARTED AT PORT 5000")
+);
